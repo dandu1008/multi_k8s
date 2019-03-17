ace5f829ab0933c8c1087c132f83876d4634a11c
dandu1008 <dandu.8x@gmail.com>
2019-03-17 18:42:49 +0000

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t dandu1008/multi-client:latest -t dandu1008/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t dandu1008/multi-server:latest -t dandu1008/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t dandu1008/multi-worker:latest -t dandu1008/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push dandu1008/multi-client:latest
+docker push dandu1008/multi-server:latest
+docker push dandu1008/multi-worker:latest
+
+docker push dandu1008/multi-client:$SHA
+docker push dandu1008/multi-server:$SHA
+docker push dandu1008/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment server=dandu1008/multi-client:$SHA
+kubectl set image deployments/server-deployment server=dandu1008/multi-server:$SHA
+kubectl set image deployments/worker-deployment server=dandu1008/multi-worker:$SHA
